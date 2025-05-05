a1cfeec914f5b9ba71b39f6fcd3c4efc6f14534c
unknown <alsrud7767@naver.com>
2025-05-05 15:17:34 +0900

    add: voice

---
+++ Assets/Resources/Audio/AudioManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("설정")]
+    public string audioFolderPath = "Audio/VoiceResources";
+    public AudioSource voiceAudioSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 선택 사항
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void PlayVoice(string relativePath)
+    {
+        Debug.Log("PlayVoice() 실행 시작"); // 최상단에 넣어보기
+
+        string fullPath = $"{audioFolderPath}/{relativePath}";
+        Debug.Log($"[AudioManager] Trying to load: {fullPath}");
+
+        AudioClip clip = Resources.Load<AudioClip>(fullPath);
+
+        if (clip == null)
+        {
+            Debug.LogError($"AudioClip not found at: {fullPath}");
+            return;
+        }
+
+        voiceAudioSource.clip = clip;
+        voiceAudioSource.Play();
+    }
+
+
+}
