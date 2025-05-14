ba46531a690ccd936c30f6c4928f286c082cc46b
nns503 <73779581+nns503@users.noreply.github.com>
2025-05-14 20:50:21 +0900

    add: Infant Cpr Scene

--- Assets/Scripts/Stage/InfantCPR/InfantCPRMessageManager.cs
+++ Assets/Scripts/Stage/InfantCPR/InfantCPRMessageManager.cs
@@ -4,16 +4,16 @@ public static class InfantCPRMessageManager
 {
     private static readonly Dictionary<InfantCPRState, string> messages = new Dictionary<InfantCPRState, string>
     {
-        { InfantCPRState.EnsureSceneSafety, "수정" },
-        { InfantCPRState.WearPPE, "수정" },
-        { InfantCPRState.CheckConsciousness, "수정" },
-        { InfantCPRState.Call119AndRequestAED, "수정" },
-        { InfantCPRState.CheckBreathingAndPulse, "수정" },
-        { InfantCPRState.Perform30ChestCompressions, "수정" },
-        { InfantCPRState.OpenAirway, "수정" },
-        { InfantCPRState.Perform2RescueBreathsWithPocketMask, "수정" },
-        { InfantCPRState.Perform5CyclesOf30To2CPR, "수정" },
-        { InfantCPRState.RecordOnMedicalChart, "수정" }
+        { InfantCPRState.EnsureSceneSafety, "현장 안전을 확인하세요." },
+        { InfantCPRState.WearPPE, "감염 방지를 위한 개인보호장구를 착용하세요." },
+        { InfantCPRState.CheckConsciousness, "의식을 확인하세요." },
+        { InfantCPRState.Call119AndRequestAED, "119 신고 및 AED를 요청하세요." },
+        { InfantCPRState.CheckBreathingAndPulse, "호흡과 맥박을 동시에 확인하세요." },
+        { InfantCPRState.Perform30ChestCompressions, "가슴압박을 30회 실시하세요." },
+        { InfantCPRState.OpenAirway, "기도를 개방하세요." },
+        { InfantCPRState.Perform2RescueBreathsWithPocketMask, "포켓마스크를 사용하여 인공호흡을 2회 실시하세요." },
+        { InfantCPRState.Perform5CyclesOf30To2CPR, "가슴압박과 인공호흡을 30 : 2로 5주기 실시하세요." },
+        { InfantCPRState.RecordOnMedicalChart, "의무기록지에 기록하세요." }
     };
 
     public static string GetMessage(InfantCPRState state)
